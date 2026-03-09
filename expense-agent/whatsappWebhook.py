8e9140d349104908722df41a9fd1b5733a6ffed1
Idrissa Conde <idrissa.conde@soprasteria.com>
2026-03-09 17:20:25 +0100

    Implement WhatsApp webhook functionality and add requirements file

--- expense-agent/whatsappWebhook.py
+++ expense-agent/whatsappWebhook.py
@@ -1,18 +1,30 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
-from twilio.rest import Client
-from dotenv import load_dotenv
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-load_dotenv()
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
+from fastapi import FastAPI, Form
+from fastapi.responses import Response
+from graph import graph
 
-message = client.messages.create(
-    from_="whatsapp:+14155238886",
-    body="Hello, there!",
-    to="whatsapp:+33758732741",
-)
+app = FastAPI()
 
-print(message.body)
+
+@app.get("/")
+def root():
+    return {"status": "agent running"}
+
+
+@app.post("/whatsapp")
+async def whatsapp_webhook(Body: str = Form(...)):
+
+    print("Message reçu :", Body)
+
+    state = {
+        "message": Body
+    }
+
+    result = graph.invoke(state)
+
+    response = """
+<Response>
+<Message>Message reçu et traité ✅</Message>
+</Response>
+"""
+
+    return Response(content=response, media_type="application/xml")
