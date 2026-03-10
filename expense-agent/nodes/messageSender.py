8f930190f4ab0c482c6c3d2145d89f93e3b7b9de
Idrissa Conde <idrissa.conde@soprasteria.com>
2026-03-10 16:13:16 +0100

    Refactor message generation in send_message function for improved clarity and intent handling

--- expense-agent/nodes/messageSender.py
+++ expense-agent/nodes/messageSender.py
@@ -10,21 +10,20 @@ def send_message(state: AgentState):
     prompt = f"""
 You are a helpful assistant that sends messages to the user.
 
-Generate a message to the user based on the result of the SQL query and the original message.
+Generate a message to the user based on the intent of the user,the result of the sql_query and the result.
 If the SQL query result is empty, it means there are no expenses matching the user's request. 
 In this case, generate a message informing the user that no expenses were found.
 You should take into account the intent of the user.
 You should take into account the sql query that was generated.
-The amount should be formatted with 2 decimal places and the euro symbol, for example: "20.00 €".
+The amount,if exists, should be formatted with 2 decimal places and the euro symbol, for example: "20.00 €".
 
 The message should be concise and short, ideally one or two sentences at most.
 
-User request: {state["message"]}
-state result: {state["result"]}
+
+result: {state["result"]}
 User intent: {state["user_intent"]}
 SQL query: {state["sql_query"]}
 
-According to SQL query result, generate a message to the user.
 Don't forget that state result can be "Query executed successfully", which means that the user intent has been fulfilled. In this case, you should generate a message confirming that the expense has been added or the request has been fulfilled.
 """  
     response = client.chat.completions.create(
